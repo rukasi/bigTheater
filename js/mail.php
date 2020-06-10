30cacf48298c17186f6812b1876774a22e0bf07c
rukasi <fet_nba@mail.ru>
2020-06-10 17:18:06 +0300

    Update mail.php

--- js/mail.php
+++ js/mail.php
@@ -2,11 +2,11 @@
 if($_POST)
     {
         $to = "it@murmanpuppet.ru"; //куда отправлять письмо
-        $from = $_POST['email']; // от кого
+        $from = "it@murmanpuppet.ru"; // от кого
         $subject = "Заявка с сайта"; //тема
         $message = 'Имя: '.$_POST['name'].'; Телефон: '.$_POST['phone'].'; Компания: ' . $_POST['company'] .'; Сообщение: ' . $_POST['message'] .';';
         $headers = "Content-type: text/html; charset=UTF-8 \r\n";
-        $headers .= "From: <почта@mail.ru>\r\n";
+        $headers .= "From: <it@murmanpuppet.ru>\r\n";
         $result = mail($to, $subject, $message, $headers);
     
         if ($result){
