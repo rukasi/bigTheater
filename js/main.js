ed1da70d06109620d3b8ab5db8d7ab2b3f39aa48
rukasi <fet_nba@mail.ru>
2020-06-10 20:33:12 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
 
     function ajaxFormRequest() {
         $.ajax({
-            url: "js/mail.php",
+            url: "mail.php",
             type: "POST",
             dataType: "html",
             data: $('#mForm').serialize(),
