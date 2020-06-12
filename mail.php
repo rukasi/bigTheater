22bd4c2e277a34448fa46e9dae689738612f0f34
rukasi <fet_nba@mail.ru>
2020-06-12 11:12:01 +0300

    add $from

--- mail.php
+++ mail.php
@@ -1,11 +1,12 @@
 <?php
 if($_POST)
     {
-        $to = "it@murmanpuppet.ru";
-        $subject = "Заявка с сайта";
+        $to = 'it@murmanpuppet.ru';
+        $from = 'it@murmanpuppet.ru'
+        $subject = 'Заявка с сайта';
         $message = 'Имя: '.$_POST['name'].'; Телефон: '.$_POST['phone'].'; Компания: ' . $_POST['company'] .'; Сообщение: ' . $_POST['message'] .';';
-        $headers = "Content-type: text/html; charset=UTF-8 \r\n";
-        $headers .= "From: <it@murmanpuppet.ru>\r\n";
+        $headers = 'Content-type: text/html; charset=UTF-8' . '\r\n';
+        $headers .= 'From: <'. $from .'>' . '\r\n';
         $result = mail($to, $subject, $message, $headers);
     
         if ($result){
